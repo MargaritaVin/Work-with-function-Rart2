0f8aeab2918e8775b63a670b296674cdb9dcbe7b
MargaritaVin <ritasergienko@rambler.ru>
2022-08-05 18:35:40 +0300

    added function sumTwoSmallestNumbers in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+function sumTwoSmallestNumbers(...args) {
+    if (args.length < 2) {
+        return console.error("Error");
+    } else {
+        return args.sort((a, b) => a - b).slice(0, 2).reduce((a, b) => a + b, 0);
+    };
+
+};
+console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77)); 
+console.log(sumTwoSmallestNumbers(1, 3, 2));
+console.log(sumTwoSmallestNumbers(1));
