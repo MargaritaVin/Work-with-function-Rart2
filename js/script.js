17e15fe4ca232337bf9fd22dc4d07f0f8f54cf3a
MargaritaVin <ritasergienko@rambler.ru>
2022-08-09 18:05:22 +0300

    added function createCalculator in script.js

--- js/script.js
+++ js/script.js
@@ -6,8 +6,35 @@ function sumTwoSmallestNumbers(...args) {
     } else {
         return args.sort((a, b) => a - b).slice(0, 2).reduce((a, b) => a + b, 0);
     };
-
 };
 console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77)); 
 console.log(sumTwoSmallestNumbers(1, 3, 2));
 console.log(sumTwoSmallestNumbers(1));
+
+
+function createCalculator(x) {
+    return {
+        sum: function(y) {
+            let sum = x + y;
+            return x = sum;
+        },
+        mult: function(y) {
+            let mult = x * y;
+            return x = mult;
+        },
+        sub: function(y) {
+            let sub = x - y;
+            return x = sub;
+        },
+        div: function(y) {
+            let div = x / y;
+            return x = div;
+        },
+    };  
+};
+const calc = createCalculator(10);
+
+console.log(calc.sum(5)); 
+console.log(calc.mult(10)); 
+console.log(calc.sub(40)); 
+console.log(calc.div(10)); 
